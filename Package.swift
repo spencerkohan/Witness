299906520175169246ecb7f75e852f025702432a
Spencer Kohan <spencerkohan@gmail.com>
2019-05-21 09:48:15 +0200

    added suopport for swift 5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 
 import PackageDescription
 
@@ -19,5 +19,5 @@ let package = Package(
             name: "WitnessTests",
             dependencies: ["Witness"]),
     ],
-    swiftLanguageVersions: [.v4, .v4_2]
+    swiftLanguageVersions: [.v4, .v4_2, .v5]
 )
